194424f61287c94ea3d928f79f70e914705cb0d5
Mansi <mansi@test.com>
2026-04-24 16:37:47 +0530

    change

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,15 +2,15 @@
 
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const{validateReview, isLoggedIn, isReviewAuthor} = require('../middleware');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 const Campground = require('../models/campground.js');
 const Review = require('../models/review.js');
 const reviews = require('../controllers/reviews');
 const ExpressError = require('../utils/ExpressError.js');
 const catchAsync = require('../utils/catchAsync.js');
 
-router.post('/',isLoggedIn, validateReview, catchAsync(reviews.createReview));
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
-router.delete('/:reviewId',isLoggedIn,isReviewAuthor, catchAsync(reviews.deleteReview));
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
 
-module.exports = router;
+// module.exports = router;
